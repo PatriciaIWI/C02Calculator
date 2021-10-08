1a21bac3ef6f75ba4866639badbb581e3c5c8c7e
PatriciaIWI <patricia.neuherz@gmx.at>
2021-10-08 19:58:56 +0200

    insert Variable

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator {
     public static void main(String[] args) {
 
+        int i = 1;
+        System.out.println("i = " + i);
+                double d = 2.3;
+        System.out.println("d = " + d);
+                boolean b = true;
+        System.out.println("b = " + b);
+                char c = 'C';
+        System.out.println("c = " + c);
+                String Tanga = "Stringtanga";
+        System.out.println(Tanga);
     }
 }
