5cf4b03865416f080dd45771d352d36a48e5a580
PatriciaIWI <patricia.neuherz@gmx.at>
2021-10-08 20:22:17 +0200

    insert Variable

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
 
         int i = 1;
         System.out.println("i = " + i);
-                double d = 2.3;
+        double d = 2.3;
         System.out.println("d = " + d);
-                boolean b = true;
+        boolean b = true;
         System.out.println("b = " + b);
-                char c = 'C';
+        char c = 'C';
         System.out.println("c = " + c);
-                String Tanga = "Stringtanga";
+        String Tanga = "Stringtanga";
         System.out.println(Tanga);
+
+        boolean Gerade = 2 % 2 == 0;
+        System.out.println("Gerade = " + Gerade);
+
     }
+
+    public static void main(String[] args) {
+
+            System.out.println("Bitte geben Sie eine Zahl ein");
+            Scanner ZahlA = new Scanner();
+            int A = ZahlA.nextInt();
+
+            System.out.println("Bitte geben Sie eine 2. Zahl ein");
+            Scanner ZahlB = new Scanner();
+            int B = ZahlB.nextInt();
+
+            int Summe = ZahlA + ZahlB;
+            System.out.println("Die Summe lautet" + Summe);
+        }
+
 }
